1539a527921b21f154f336ed5c34cb8858ff4669
carlos5veiga <carlos5veiga@gmail.com>
2022-03-15 21:18:55 -0300

    Building records table structure

--- front-web/src/pages/Records/index.tsx
+++ front-web/src/pages/Records/index.tsx
@@ -1,7 +1,47 @@
 import React from "react";
+import './styles.css';
 
 const Records = () => (
-    <h1>Records</h1>
+    <div className="page-container">
+        <table className="records-table" cellPadding="0" cellSpacing="0">
+            <thead>
+                <tr>
+                    <th>INSTANTE</th>
+                    <th>NOME</th>
+                    <th>IDADE</th>
+                    <th>PLATAFORMA</th>
+                    <th>GÊNERO</th>
+                    <th>TÍTULO DO GAME</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>20/08/2020 13:45</td>
+                    <td>Carlinhos Veiga</td>
+                    <td>47</td>
+                    <td>XBOX</td>
+                    <td>Ação - Aventura</td>
+                    <td>The las of US</td>
+                </tr>
+                <tr>
+                    <td>20/08/2020 13:45</td>
+                    <td>Carlinhos Veiga</td>
+                    <td>47</td>
+                    <td>XBOX</td>
+                    <td>Ação - Aventura</td>
+                    <td>The las of US</td>
+                </tr>
+                <tr>
+                    <td>20/08/2020 13:45</td>
+                    <td>Carlinhos Veiga</td>
+                    <td>47</td>
+                    <td>XBOX</td>
+                    <td>Ação - Aventura</td>
+                    <td>The las of US</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
 );
 
 export default Records;
