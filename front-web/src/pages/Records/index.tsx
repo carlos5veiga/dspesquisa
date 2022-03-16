52fe1db5a4f16eb0585fe9693e82020632dc5e87
carlos5veiga <carlos5veiga@gmail.com>
2022-03-16 09:48:44 -0300

    Seed and format records table

--- front-web/src/pages/Records/index.tsx
+++ front-web/src/pages/Records/index.tsx
@@ -1,47 +1,50 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from 'axios';
 import './styles.css';
+import { RecordsResponse } from "./types";
+import { formatDate } from "./helpers";
 
-const Records = () => (
-    <div className="page-container">
-        <table className="records-table" cellPadding="0" cellSpacing="0">
-            <thead>
-                <tr>
-                    <th>INSTANTE</th>
-                    <th>NOME</th>
-                    <th>IDADE</th>
-                    <th>PLATAFORMA</th>
-                    <th>GÊNERO</th>
-                    <th>TÍTULO DO GAME</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>20/08/2020 13:45</td>
-                    <td>Carlinhos Veiga</td>
-                    <td>47</td>
-                    <td>XBOX</td>
-                    <td>Ação - Aventura</td>
-                    <td>The las of US</td>
-                </tr>
-                <tr>
-                    <td>20/08/2020 13:45</td>
-                    <td>Carlinhos Veiga</td>
-                    <td>47</td>
-                    <td>XBOX</td>
-                    <td>Ação - Aventura</td>
-                    <td>The las of US</td>
-                </tr>
-                <tr>
-                    <td>20/08/2020 13:45</td>
-                    <td>Carlinhos Veiga</td>
-                    <td>47</td>
-                    <td>XBOX</td>
-                    <td>Ação - Aventura</td>
-                    <td>The las of US</td>
-                </tr>
-            </tbody>
-        </table>
-    </div>
-);
+const BASE_URL = 'http://localhost:8080'
+
+const Records = () => {
+
+    const [recordsResponse, setRecordsResponse] = useState<RecordsResponse>();
+    console.log(recordsResponse);
+
+    useEffect(() => {
+        axios.get(`${BASE_URL}/records?linesPerPage=12`)
+            .then(response => setRecordsResponse(response.data));
+    }, []);
+
+
+    return (
+        <div className="page-container">
+            <table className="records-table" cellPadding="0" cellSpacing="0">
+                <thead>
+                    <tr>
+                        <th>INSTANTE</th>
+                        <th>NOME</th>
+                        <th>IDADE</th>
+                        <th>PLATAFORMA</th>
+                        <th>GÊNERO</th>
+                        <th>TÍTULO DO GAME</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {recordsResponse?.content.map(record => (
+                        <tr key={record.id}>
+                            <td>{formatDate(record.moment)}</td>
+                            <td>{record.name}</td> 
+                            <td>{record.age}</td>
+                            <td className="text-secondary">{record.gamePlatform}</td>
+                            <td>{record.genreName}</td>
+                            <td className="text-primary">{record.gameTitle}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    );
+};
 
 export default Records;
