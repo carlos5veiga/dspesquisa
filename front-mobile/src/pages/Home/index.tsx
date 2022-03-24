d2cef1ac772397d36565f8ab7d55c551f8a1d24e
carlos5veiga <carlos5veiga@gmail.com>
2022-03-24 07:09:39 -0300

    Adding routes

--- front-mobile/src/pages/Home/index.tsx
+++ front-mobile/src/pages/Home/index.tsx
@@ -1,15 +1,20 @@
 import React from "react";
-import { FontAwesome5 as Icon } from '@expo/vector-icons'
-import { Text, StyleSheet, View, Image, Alert } from "react-native";
-import { RectButton } from 'react-native-gesture-handler';
-
-const handleOnPress = () => {
-    Alert.alert('Você clicou no botão!')
-}
+import { FontAwesome5 as Icon } from "@expo/vector-icons";
+import { Text, StyleSheet, View, Image } from "react-native";
+import { RectButton } from "react-native-gesture-handler";
+import Header from "../../components/Header";
+import { useNavigation } from "@react-navigation/native";
 
 const Home = () => {
+  const navigation = useNavigation();
+
+  const handleOnPress = () => {
+    navigation.navigate("CreateRecord");
+  }
+
   return (
     <>
+      <Header />
       <View style={styles.container}>
         <Image
           source={require("../../assets/gamer.png")}
@@ -19,16 +24,14 @@ const Home = () => {
         <Text style={styles.subTitle}>Nos diga qual é seu jogo favorito!</Text>
       </View>
       <View style={styles.footer}>
-          <RectButton style={styles.button} onPress={handleOnPress}>
-              <Text style={styles.buttonText}>
-                  COLETAR DADOS
-              </Text>
-              <View style={styles.buttonIcon}>
-                  <Text>
-                    <Icon name="chevron-right" color="#FFF" size={25}/>
-                  </Text>
-              </View>
-          </RectButton>
+        <RectButton style={styles.button} onPress={handleOnPress}>
+          <Text style={styles.buttonText}>COLETAR DADOS</Text>
+          <View style={styles.buttonIcon}>
+            <Text>
+              <Icon name="chevron-right" color="#FFF" size={25} />
+            </Text>
+          </View>
+        </RectButton>
       </View>
     </>
   );
